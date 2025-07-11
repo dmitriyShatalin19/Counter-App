b23b4b00b713cc6e8e132158cd0cfde6993ea239
dmitriyShatalin19 <shatal1d@yandex.ru>
2025-07-11 11:15:56 +0300

    добавил кнопку -1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-let button = document.querySelector('.button'); // позволяте вытаскиывать элемент из index.html в script.js
-let buttonReset = document.querySelector('.buttonReset');
+let button = document.querySelector('.js-buttonPlus'); // позволяте вытаскиывать элемент из index.html в script.js
+let buttonReset = document.querySelector('.js-buttonReset');
+let buttonMinus = document.querySelector('.js-buttonMinus')
 let counterText = document.querySelector('.counter');
 let counter = 0;
 
@@ -7,6 +8,13 @@ button.addEventListener('click', function() {
     counter ++;
     counterText.innerText = counter;
 });
+
+buttonMinus.addEventListener('click', function() {
+    if (counter > 0) {
+        counter --;
+        counterText.innerText = counter;
+    }
+})
 
 buttonReset.addEventListener('click', function() {
     counter = 0;
