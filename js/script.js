b74ac688de6500558a49b3f384bc22e00da053a5
dmitriyShatalin19 <shatal1d@yandex.ru>
2025-07-12 13:04:02 +0300

    поменял цвет и фон, сделал ограничение

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
-let button = document.querySelector('.js-buttonPlus'); // позволяте вытаскиывать элемент из index.html в script.js
+let button = document.querySelector('.js-buttonPlus');
 let buttonReset = document.querySelector('.js-buttonReset');
-let buttonMinus = document.querySelector('.js-buttonMinus')
+let buttonMinus = document.querySelector('.js-buttonMinus');
 let counterText = document.querySelector('.counter');
 let counter = 0;
 
+const minCounter = 0;
+const maxCounter = 50;
+
 button.addEventListener('click', function() {
-    counter ++;
+  if (counter < maxCounter) {
+    counter++;
     counterText.innerText = counter;
+  } else if (counter === maxCounter) {
+    alert('Это предел!')
+  }
 });
 
 buttonMinus.addEventListener('click', function() {
-    if (counter > 0) {
-        counter --;
-        counterText.innerText = counter;
-    }
-})
-
-buttonReset.addEventListener('click', function() {
-    counter = 0;
+  if (counter > minCounter) {
+    counter--;
     counterText.innerText = counter;
+  }
 });
 
-
+buttonReset.addEventListener('click', function() {
+  counter = 0;
+  counterText.innerText = counter;
+});
